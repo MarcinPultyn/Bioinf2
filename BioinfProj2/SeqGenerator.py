ad362bd262ae733ad14203d833d047f5f2125a4a
karanpo <karolina_ania@o2.pl>
2018-01-21 20:38:16 +0100

    seq generator

--- BioinfProj2/SeqGenerator.py
+++ BioinfProj2/SeqGenerator.py
@@ -49,19 +49,21 @@ def getMutation(x, probList):
     val = probList[0]
     for i in range(1, len(probList)):
         if x < val:
-            return getColName(val-1)
+            print ("i: %d, x: %.4f, val: %.4f" % (i,x,val) )
+            return getColName(i-1)
         val += probList[i]
 
 def mutateSequence(seq, probMatrix):
     for i in range(0, len(seq)):
         x = random.random()
-        if seq[i] == 'a':
+        if seq[i] == "a":
             seq[i] = getMutation(x, probMatrix[0])
-        elif seq[i] == 'c':
+        elif seq[i] == "c":
             seq[i] = getMutation(x, probMatrix[1])
-        elif seq[i] == 'g':
+        elif seq[i] == "g":
             seq[i] = getMutation(x, probMatrix[2])
-        elif seq[i] == 't':
+        elif seq[i] == "t":
+            print (probMatrix[3])
             seq[i] = getMutation(x, probMatrix[3])
         
     return seq
@@ -71,8 +73,10 @@ b = 0.02
 seqLength = 25
 
 t = int(input("Podaj czas t "))
-seq1 = seq2 = randomDnaSequence(seqLength)
-print ("seq: \n")
+seq = randomDnaSequence(seqLength)
+seq1 = seq2 = list(seq)
+print (seq)
+print ("\n seq: \n")
 print (seq1)
 print ("\n")
 print (seq1[0])
@@ -87,15 +91,15 @@ print ("\n")
 print (retSeq2)
 
 file = open("seq1.txt","w") 
-file.write(seq1) 
+file.write(''.join(seq1)) 
 file.close()
 
 file = open("seq2.txt","w")  
-file.write(seq2) 
+file.write(''.join(seq2)) 
 file.close()
 
-#file = open("matrix.txt","w") 
-#file.write(probMatrix) 
-#file.close()
+file = open("matrix.txt","w") 
+file.write(''.join(probMatrix))
+file.close()
 
         
